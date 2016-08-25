a9d5535297b051bbebf76cfd909e2718f3a29b8e
Aniela M Wolkonowski <anielamw@gmail.com>
2016-08-25 16:38:51 -0400

    created initailize method and outlined needed Team Class Methods

--- models/team.rb
+++ models/team.rb
@@ -1,5 +1,17 @@
 require_relative "../lib/team_data"
 
 class Team
+  attr_reader :name
 
+  #Create a Team object with just the name as an argument
+  def initialize(name)
+    @name = name
+    #Create an empty array for the players objects to be stored
+    @players = []
+  end
+
+  #Create the Players objects
+  def create_player_objects
+    #iterate over the players on a given team and append ne player objects to the end of the @players array
+  end
 end
